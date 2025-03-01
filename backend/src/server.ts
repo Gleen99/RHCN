b072cda55544209c59da91bed99e04186beba70e
gleen99 <darsycamille2@gmail.com>
2025-03-01 02:23:17 +0100

    Style modif package.json

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,4 +1,6 @@
 import { connectToDatabase, standardExpressRun} from './helpers/init';
+import dotenv from 'dotenv';
+dotenv.config({ path: '.env.prod' });
 
 connectToDatabase();
 standardExpressRun();
