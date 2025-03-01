85a5c190d7bb211baea0bc3a2dc931e1d47da366
gleen99 <darsycamille2@gmail.com>
2025-03-01 03:26:06 +0100

    Style modif package.json

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,6 +1,13 @@
 import { connectToDatabase, standardExpressRun} from './helpers/init';
-require('dotenv').config();
+
+
 console.log('Environnement:', process.env.NODE_ENV);
+const dotenv = require('dotenv');
+const path = require('path');
+
+dotenv.config({
+    path: path.resolve(__dirname, '../.env.production')
+});
 
 connectToDatabase();
 standardExpressRun();
