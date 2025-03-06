f5646dfb60454056c724e787635f4f1274235005
gleen99 <darsycamille2@gmail.com>
2025-03-06 22:47:21 +0100

    modif local

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -1,13 +1,4 @@
 import { connectToDatabase, standardExpressRun} from './helpers/init';
 
-
-console.log('Environnement:', process.env.NODE_ENV);
-const dotenv = require('dotenv');
-const path = require('path');
-
-dotenv.config({
-    path: path.resolve(__dirname, '../.env.prod')
-});
-
 connectToDatabase();
 standardExpressRun();
