3e206357d83f04bf69df46b328a29e91625be690
gleen99 <darsycamille2@gmail.com>
2025-03-21 20:19:29 +0100

    correction

--- backend/src/helpers/express.ts
+++ backend/src/helpers/express.ts
@@ -14,6 +14,7 @@ export function expressInit(): Application {
     app.use('/public', express.static('public'));
     app.use(helmet());
     app.disable("etag");
+
     app.use((req, res, next) => {
         res.setHeader(
             "Content-Security-Policy",
